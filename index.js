92cff07724ee2ab99e1b6f2dba8bdd85c83d23d2
Codersera <info@codersera.com>
2019-07-05 09:27:39 +0100

    adding the script

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+const exec = require('child_process').exec;
 const fs = require('fs');
 const path = require('path');
 const regex = /cheap-module-source-map/g;
 
+const packageFile = require(path.resolve(__dirname, '../package.json'));
 function replaceFileContent(fileName, oldNameRegex, newName) {
     console.log(`${fileName} parsing file`);
-    let fileContent = fs.readFileSync(fileName, 'utf8');
 
+    let fileContent = fs.readFileSync(fileName, 'utf8');
     fileContent = fileContent.replace(oldNameRegex, newName);
     fs.writeFileSync(fileName, fileContent);
     console.log(`${fileName} updated successfully`);
-}
 
-replaceFileContent(path.resolve(__dirname, './node_modules/react-scripts/config/webpack.config.js'), regex, 'eval-source-map');
+}
+replaceFileContent(path.resolve(__dirname, '../node_modules/react-scripts/config/webpack.config.js'), regex, 'eval-source-map');
+
+packageFile.scripts.postinstall = 'patch-package';
+packageFile.dependencies['patch-package'] = packageFile.dependencies['patch-package'] || "^6.1.2";
+
+fs.writeFileSync(path.resolve(__dirname, '../package.json'), JSON.stringify(packageFile, null, '\t'));
