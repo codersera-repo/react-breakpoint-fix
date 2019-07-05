e808851b38b213bb19ee83f625c4023d22c16426
Codersera <info@codersera.com>
2019-07-05 11:17:56 +0100

    adding the execsync commands to run the commands from the js file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const exec = require('child_process').exec;
+#!/usr/bin/env node
+
+const execSync = require('child_process').execSync;
 const fs = require('fs');
 const path = require('path');
 const regex = /cheap-module-source-map/g;
@@ -11,12 +13,19 @@ function replaceFileContent(fileName, oldNameRegex, newName) {
     fileContent = fileContent.replace(oldNameRegex, newName);
     fs.writeFileSync(fileName, fileContent);
     console.log(`${fileName} updated successfully`);
-
 }
-replaceFileContent(path.resolve(__dirname, '../react-scripts/config/webpack.config.js'), regex, 'eval-source-map');
 
-packageFile.scripts.postinstall = 'patch-package';
 packageFile.dependencies['patch-package'] = packageFile.dependencies['patch-package'] || "^6.1.2";
 packageFile.scripts['patch-react-scripts'] = 'patch-package react-scripts';
+fs.writeFileSync(path.resolve(__dirname, '../../package.json'), JSON.stringify(packageFile, null, '\t'));
+
+console.log('installing patch-package, please wait, might take some time.');
+execSync('npm install patch-package@6.1.2 --save');
+
+packageFile.scripts.postinstall = 'patch-package';
+replaceFileContent(path.resolve(__dirname, '../react-scripts/config/webpack.config.js'), regex, 'eval-source-map');
 
 fs.writeFileSync(path.resolve(__dirname, '../../package.json'), JSON.stringify(packageFile, null, '\t'));
+console.log('patching your packages, please wait');
+execSync('patch-package react-scripts');
+console.log('Done! React breakpoint issues should have been sorted by now.');
