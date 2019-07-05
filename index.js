07ab0471141900da3a0f3f022bb87f6c26db4b63
Codersera <info@codersera.com>
2019-07-05 10:34:26 +0100

    updating the version and some fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const regex = /cheap-module-source-map/g;
 
-const packageFile = require(path.resolve(__dirname, '../package.json'));
+const packageFile = require(path.resolve(__dirname, '../../package.json'));
 function replaceFileContent(fileName, oldNameRegex, newName) {
     console.log(`${fileName} parsing file`);
 
@@ -13,9 +13,10 @@ function replaceFileContent(fileName, oldNameRegex, newName) {
     console.log(`${fileName} updated successfully`);
 
 }
-replaceFileContent(path.resolve(__dirname, '../node_modules/react-scripts/config/webpack.config.js'), regex, 'eval-source-map');
+replaceFileContent(path.resolve(__dirname, '../react-scripts/config/webpack.config.js'), regex, 'eval-source-map');
 
 packageFile.scripts.postinstall = 'patch-package';
 packageFile.dependencies['patch-package'] = packageFile.dependencies['patch-package'] || "^6.1.2";
+packageFile.scripts['patch-react-scripts'] = 'patch-package react-scripts';
 
-fs.writeFileSync(path.resolve(__dirname, '../package.json'), JSON.stringify(packageFile, null, '\t'));
+fs.writeFileSync(path.resolve(__dirname, '../../package.json'), JSON.stringify(packageFile, null, '\t'));
