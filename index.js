e9dd9cbc8ae71313bb32144870e7ccbca7beb847
Codersera <info@codersera.com>
2019-07-05 11:21:28 +0100

    writing output to console

--- index.js
+++ index.js
@@ -20,12 +20,14 @@ packageFile.scripts['patch-react-scripts'] = 'patch-package react-scripts';
 fs.writeFileSync(path.resolve(__dirname, '../../package.json'), JSON.stringify(packageFile, null, '\t'));
 
 console.log('installing patch-package, please wait, might take some time.');
-execSync('npm install patch-package@6.1.2 --save');
+execSync('npm install patch-package@6.1.2 --save',
+    {stdio: 'inherit'});
 
 packageFile.scripts.postinstall = 'patch-package';
 replaceFileContent(path.resolve(__dirname, '../react-scripts/config/webpack.config.js'), regex, 'eval-source-map');
 
 fs.writeFileSync(path.resolve(__dirname, '../../package.json'), JSON.stringify(packageFile, null, '\t'));
 console.log('patching your packages, please wait');
-execSync('patch-package react-scripts');
+execSync('patch-package react-scripts',
+    {stdio: 'inherit'});
 console.log('Done! React breakpoint issues should have been sorted by now.');
